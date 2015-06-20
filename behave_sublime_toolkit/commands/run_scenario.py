0ee4848dfa4b13c764349b06f160afddc79c3bac
mixxorz <mixxorz@gmail.com>
2015-06-20 21:16:55 +0800

    Running behave scenarios under the cursor works

--- behave_sublime_toolkit/commands/run_scenario.py
+++ behave_sublime_toolkit/commands/run_scenario.py
@@ -9,15 +9,28 @@ from ..utils.log import OutputPanelMixin
 
 class BstRunScenario(sublime_plugin.TextCommand, OutputPanelMixin):
 
+    '''
+    Runs the behave scenario under the cursor
+    '''
+
     def run(self, edit, **kwargs):
-        current_line_number = self.view.rowcol(
-            self.view.sel()[0].begin())[0] + 1
-        sublime.status_message('Line number: %d' % current_line_number)
+        args = []
+
         python = self.view.settings().get('python_interpreter', 'python')
+        args.append(python)
 
         behave = os.path.join(os.path.dirname(python), 'behave')
+        args.append(behave)
 
-        with subprocess.Popen([python, behave],
+        if 'gherkin' in self.view.scope_name(0):
+            current_file = self.view.file_name()
+            current_line_number = self.view.rowcol(
+                self.view.sel()[0].begin())[0] + 1
+
+            scenario = '%s:%d' % (current_file, current_line_number)
+            args.append(scenario)
+
+        with subprocess.Popen(args,
                               stdout=subprocess.PIPE,
                               bufsize=1,
                               universal_newlines=True,
@@ -26,6 +39,3 @@ class BstRunScenario(sublime_plugin.TextCommand, OutputPanelMixin):
                 self.append(edit, line, end='')
 
             self.append(edit, 'Exit code: %d' % p.wait())
-
-        # self.append(edit, 'Helloffffsdfsdfsdfsdfsdfsdfsfsdfsdfsdf')
-        # self.append(edit, 'Number 2')
