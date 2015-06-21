f16af8f1b8e6238c22715f0c4fa7bd2faa807880
mixxorz <mixxorz@gmail.com>
2015-06-21 10:49:30 +0800

    Removed old packages

--- behave_sublime_toolkit/commands/__init__.py
+++ behave_sublime_toolkit/commands/__init__.py
@@ -1,9 +1,5 @@
-from .hello import BstHello
-from .log import BstDisplayPanel
 from .run_scenario import BstRunScenario
 
 _all__ = [
-    'BstDisplayPanel',
-    'BstHello',
     'BstRunScenario'
 ]
