ee3dcbb277bd1a4e8ed90bc1acb6ac117c0a6b6b
mixxorz <mixxorz@gmail.com>
2015-06-24 11:23:37 +0800

    Use new API for go_to_step_definition

--- behave_toolkit/commands/go_to_step_definition.py
+++ behave_toolkit/commands/go_to_step_definition.py
@@ -1,18 +1,15 @@
 import os
-import re
 
 import sublime
 import sublime_plugin
 
 from ..behave_command import BehaveCommand
+from ..utils import parser
 from ..utils.scope import is_gherkin
 
 
 class BstGoToStepDefinition(sublime_plugin.TextCommand, BehaveCommand):
-
-    '''
-    Go to step definition for the step under the cursor.
-    '''
+    """Go to step definition for the step under the cursor"""
 
     def run(self, edit):
         sublime.set_timeout_async(self.run_async, 0)
@@ -24,44 +21,38 @@ class BstGoToStepDefinition(sublime_plugin.TextCommand, BehaveCommand):
         current_line_number = self.view.rowcol(
             self.view.sel()[0].begin())[0] + 1
 
-        step = '%s:%s' % (current_file, current_line_number)
+        query_location = '%s:%s' % (current_file, current_line_number)
 
-        output = self.behave(self.view.file_name(),
-                             '--dry-run',
-                             '--format',
-                             'steps.usage',
-                             '--no-summary',
-                             '--no-snippets')
+        step_data = self._get_step_data()
 
-        undefined_offset = output.find('UNDEFINED STEPS')
-        offset = output.find(step)
+        used_steps = parser.parse_used_steps(step_data)
+        unused_steps = parser.parse_unused_steps(step_data)
 
-        # If UNDEFINED STEPS is found, make sure we have a valid offset
-        if undefined_offset != -1:
-            if offset > undefined_offset or offset == -1:
-                sublime.status_message('Definition not found for "%s"' % step)
-                return
+        all_steps = used_steps + unused_steps
 
-        output = output[:offset]
+        matched_step = None
+        for step in all_steps:
+            if matched_step:
+                break
+            for usage in step.usages:
+                usage_location = '%s:%d' % (usage.path, usage.line)
+                if usage_location == query_location:
+                    matched_step = step
+                    break
 
-        p = re.compile('(^@.*)', re.MULTILINE)
-        last_match = re.findall(p, output)[-1]
-        m = re.search('^@.*#(.*)$', last_match)
+        if matched_step:
+            absolute_path = os.path.join(
+                current_root,
+                matched_step.path)
 
-        file_path = m.group(1).strip()
+            absolute_path += ':%d' % matched_step.line
 
-        absolute_path = os.path.join(
-            current_root,
-            file_path)
-
-        self.view.window().open_file(
-            absolute_path, sublime.ENCODED_POSITION)
-
-        # TODO: Move the cursor position to highlight the step definition
-        # better
+            self.view.window().open_file(absolute_path,
+                                         sublime.ENCODED_POSITION)
+        else:
+            sublime.status_message(
+                'Definition not found for: "%s"' % query_location)
 
     def is_enabled(self):
-        '''
-        Enable only for Gherkin
-        '''
+        """Enable only for Gherkin"""
         return is_gherkin(self.view)
